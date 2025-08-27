6c2c6edfc139cc56a8d6b4843d6c10cf66fa7ef4
sarahvue <sarahvue0924@gmail.com>
2025-08-26 21:58:13 -0700

    added components

---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function NavBar() {
+  return (
+    <div id="navbar">
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    </div>
+  );
+}
+export default NavBar;
